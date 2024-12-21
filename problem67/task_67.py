10db5c20feb0764c8c7f7393b45f33a18abd3ac6
neal <neal.amratia@sportlight.ai>
2024-12-21 21:57:36 +0000

    Problem 67 solution

---
+++ problem67/task_67.py
@@ -0,0 +1,24 @@
+# Read the triangle data from a file
+with open("problem67_triangle.txt", "r") as file:
+    data = file.read()
+
+# Convert to a 2D array
+pyramid = [list(map(int, line.split())) for line in data.strip().split("\n")]
+
+# Repeat until only one row is left
+while len(pyramid) > 1:
+    # Get the last two rows
+    slr = pyramid[-2]  # Second last row
+    lr = pyramid[-1]   # Last row
+    
+    # Create a new row by combining slr and lr
+    nlr = [0] * len(slr)
+    for i in range(len(slr)):
+        nlr[i] = max(slr[i] + lr[i], slr[i] + lr[i + 1])
+    
+    # Replace the last two rows with the new row
+    pyramid = pyramid[:-2]  # Remove the last two rows
+    pyramid.append(nlr)     # Add the new row
+
+# Print the final result
+print(pyramid[0][0])  # The maximum total
